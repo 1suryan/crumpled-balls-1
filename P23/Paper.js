b1c8d9627c216083cb2a740a84d4c31cd3fe61a9
1suryan <70902899+1suryan@users.noreply.github.com>
2020-12-22 10:06:06 -0500

    Add files via upload

---
+++ P23/Paper.js
@@ -0,0 +1,48 @@
+class Paper {
+
+constructor(x,y,r) {
+
+var options = {
+
+isStatic:false,
+restitution:0.3,
+friction:0.5,
+density:1.2
+
+
+}
+
+this.x = x;
+this.y = y;
+this.radius = r;
+this.body = Bodies.circle(this.x,this.y,r/2,options)
+this.radius = r;
+World.add(world,this.body);
+
+
+
+
+
+}
+
+display(){
+
+var pos = this.body.position;
+push();
+translate(pos.x,pos.y)
+rectMode(CENTER)
+fill("pink")
+ellipse(0,0,this.radius*2,this.radius*2)
+pop()
+
+
+}
+
+
+
+
+
+}
+
+
+
